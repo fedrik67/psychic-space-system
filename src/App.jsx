b31c734fee35a128f70f21948b5f15e9819d06e7
fedrik67 <70642982+fedrik67@users.noreply.github.com>
2025-04-21 04:34:57 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import About from './pages/About';
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+    <p>Welcome to the homepage.</p>
+    const Appa()
+  </div>
+);
 
 function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+
+
+
+const Appa= () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -9,7 +37,7 @@ function App() {
           GitHub Codespaces <span className="heart">♥️</span> React
         </p>
         <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
+          king and queen is<code>src/App.jsx</code> ready.
         </p>
         <p>
           <a
@@ -27,3 +55,4 @@ function App() {
 }
 
 export default App;
+
